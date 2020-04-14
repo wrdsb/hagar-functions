ea623b3635c5662aab1adf9775074e87fb1651d5
James Schumann <james@schumann.ca>
2020-04-14 12:15:12 -0400

    create shared/createQueueMessage.ts

---
+++ shared/createQueueMessage.ts
@@ -0,0 +1,20 @@
+import {
+    StorageSharedKeyCredential,
+    QueueServiceClient
+} from "@azure/storage-queue";
+
+async function createQueueMessage(storageAccount, storageKey, queueName, queueMessage)
+{
+    const queueSharedKeyCredential = new StorageSharedKeyCredential(storageAccount, storageKey);
+    const queueServiceClient = new QueueServiceClient(
+        `https://${storageAccount}.queue.core.windows.net`,
+        queueSharedKeyCredential
+    );
+    const queueClient = queueServiceClient.getQueueClient(queueName);
+
+    const sendMessageResponse = await queueClient.sendMessage(queueMessage);
+
+    return sendMessageResponse;
+}
+
+export { createQueueMessage };
